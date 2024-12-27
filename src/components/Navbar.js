cc3fb4b9aa0cdfaf54dfbf35cd84a63d458290fb
Vaanya Singh <vaanya277@gmail.com>
2024-12-28 02:55:18 +0530

    Vaanya

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,22 +1,59 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
+import logo from '../assets/logo.svg';
 
-function Navbar() {
+
+const Navbar = () => {
+  const location = useLocation();
+
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <Link className="navbar-brand" to="/">KaamKar</Link>
-      <div className="collapse navbar-collapse">
-        <ul className="navbar-nav ml-auto">
-          <li className="nav-item">
-            <Link className="nav-link" to="/profile">Profile</Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" to="/dashboard">Dashboard</Link>
-          </li>
-        </ul>
+      <div className="container">
+        <Link className="navbar-brand" to="/">
+          <img src={logo} alt="Logo" height="30" className="me-2" />
+          Freelance Platform
+        </Link>
+        
+        <button 
+          className="navbar-toggler" 
+          type="button" 
+          data-bs-toggle="collapse" 
+          data-bs-target="#navbarNav"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav ms-auto">
+            <li className="nav-item">
+              <Link 
+                className={`nav-link ${location.pathname === '/' ? 'active' : ''}`}
+                to="/"
+              >
+                Home
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link 
+                className={`nav-link ${location.pathname === '/dashboard' ? 'active' : ''}`}
+                to="/dashboard"
+              >
+                Dashboard
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link 
+                className={`nav-link ${location.pathname === '/profile' ? 'active' : ''}`}
+                to="/profile"
+              >
+                Profile
+              </Link>
+            </li>
+          </ul>
+        </div>
       </div>
     </nav>
   );
-}
+};
 
 export default Navbar;
